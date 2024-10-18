bce9c4b6d01f0f7ac6872443fc00aaa6d934af2e
livingstonm6 <94208409+livingstonm6@users.noreply.github.com>
2024-10-18 16:28:00 -0600

    imp: reading ROM file into memory.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,44 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    try stdout.print("Enter ROM filename:", .{});
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var input_buffer: [100]u8 = undefined;
+    var file_name: []const u8 = undefined;
 
-    try bw.flush(); // don't forget to flush!
-}
+    if (try stdin.readUntilDelimiterOrEof(input_buffer[0..], '\n')) |value| {
+        file_name = std.mem.trimRight(u8, value[0 .. value.len - 1], "\r");
+        std.log.debug("{s}", .{file_name});
+    } else {
+        std.log.debug("Error reading input.", .{});
+        return;
+    }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var list = std.ArrayList(u8).init(allocator);
+    defer list.deinit();
+
+    const file = try std.fs.cwd().openFile(file_name, .{});
+    defer file.close();
+
+    var buffered_file = std.io.bufferedReader(file.reader());
+    var buffer: [1]u8 = undefined;
+
+    while (true) {
+        const num_bytes_read = try buffered_file.read(&buffer);
+
+        if (num_bytes_read == 0) {
+            break;
+        }
+
+        try list.append(buffer[0]);
+    }
+
+    std.log.debug("list: {any}", .{list.items});
 }
