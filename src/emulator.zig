372df6f797773cc05cc7c4bca503c90142feea79
livingstonm6 <94208409+livingstonm6@users.noreply.github.com>
2024-11-13 21:18:17 -0700

    ref: Emulator to use createBus and deinitialize cart and bus.

--- src/emulator.zig
+++ src/emulator.zig
@@ -1,13 +1,15 @@
 const std = @import("std");
 const CPU = @import("cpu.zig").CPU;
-const MemoryBus = @import("bus.zig").MemoryBus;
+const createBus = @import("bus.zig").createBus;
 const initCart = @import("cart.zig").initCart;
 
 pub fn run(filename: []const u8) !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-
-    const cart = try initCart(gpa.allocator(), filename);
-    const bus = MemoryBus{ .cart = &cart };
+    const allocator = gpa.allocator();
+    const cart = try initCart(allocator, filename);
+    defer cart.deinit();
+    const bus = try createBus(&cart, allocator);
+    defer bus.deinit();
     var cpu = CPU{ .bus = &bus };
 
     while (cpu.running) {
