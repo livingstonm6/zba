ea8c606c4e7a3dd142ab5af6ea05e62f8fe8702e
livingstonm6 <94208409+livingstonm6@users.noreply.github.com>
2024-11-13 21:15:09 -0700

    imp: read BIOS data into MemoryBus.
    Implemented createBus function.
    
    Fixed memory address offset when reading cartridge data.

--- src/bus.zig
+++ src/bus.zig
@@ -1,10 +1,42 @@
+const std = @import("std");
 const Cart = @import("cart.zig").Cart;
 
 pub const MemoryBus = struct {
     cart: *const Cart,
+    allocator: std.mem.Allocator,
+    ram: []u8 = undefined,
+
+    pub fn init(self: *MemoryBus) !void {
+        const bios = try std.fs.cwd().openFile("bios.bin", .{});
+        defer bios.close();
+
+        const file_size = try bios.getEndPos();
+        self.ram = try self.allocator.alloc(u8, file_size);
+
+        var buffered_bios = std.io.bufferedReader(bios.reader());
+        var buffer: [1]u8 = undefined;
+
+        for (0..file_size) |i| {
+            const num_bytes_read = try buffered_bios.read(&buffer);
+
+            if (num_bytes_read == 0) break;
+
+            self.ram[i] = buffer[0];
+        }
+    }
+
+    pub fn deinit(self: MemoryBus) void {
+        self.allocator.free(self.ram);
+    }
 
     pub fn read8(self: MemoryBus, address: u32) u8 {
-        return self.cart.data[address];
+        if (address < 0x05000000) {
+            return self.ram[address];
+        }
+        if (address > 0x07FFFFFF and address < 0x10000000) {
+            return self.cart.data[address - 0x08000000];
+        }
+        unreachable;
     }
 
     pub fn read16(self: MemoryBus, address: u32) u16 {
@@ -21,3 +53,10 @@ pub const MemoryBus = struct {
         return lower | (@as(u32, upper) << 16);
     }
 };
+
+pub fn createBus(cart: *const Cart, allocator: std.mem.Allocator) !MemoryBus {
+    var bus = MemoryBus{ .cart = cart, .allocator = allocator };
+    try bus.init();
+
+    return bus;
+}
